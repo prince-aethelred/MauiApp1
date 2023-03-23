c0a873c171dd4357215c5ea12741b711aa967f0f
prince-aethelred <akrmrnba@gmail.com>
2023-03-23 18:05:02 -0400

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace MauiApp1;
+
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+	int count2 = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private void OnCounterClicked2(object sender, EventArgs e)
+	{ 
+		count2++;
+
+		if (count2 == 1)
+			CounterBtn2.Text = $"Yoooo Clicked {count2} time";
+		else if (count2 >= 2)
+			CounterBtn2.Text = $"Yoooo Clicked {count2} times";
+
+		SemanticScreenReader.Announce(CounterBtn2.Text);
+	}
+
+    private async void learn_more_clicked(object sender, EventArgs e)
+    {
+		// Navigate to the specified URL in the system browser
+		await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
+
